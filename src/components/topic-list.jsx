786e72b2ddbb5ee9c0a3a1ee24176ba59807f6e6
Darryl Blake <darryl@darrylblake.com>
2015-11-24 13:15:50 +1300

    Implementing actions.

--- src/components/topic-list.jsx
+++ src/components/topic-list.jsx
@@ -1,10 +1,11 @@
 var React = require('react');
 var Reflux = require('reflux');
 var TopicStore = require('../stores/topic-store');
+var Actions = require('../actions');
 
 module.exports = React.createClass({
   mixins: [
-    Reflux.listenTo(TopicStore, 'onChangexxx')
+    Reflux.listenTo(TopicStore, 'onChange')
   ],
   getInitialState: function() {
     return {
@@ -12,7 +13,7 @@ module.exports = React.createClass({
     }
   },
   componentWillMount: function() {
-    TopicStore.getTopics();
+    Actions.getTopics();
   },
   render: function() {
     return <div className="list-group">
@@ -27,7 +28,7 @@ module.exports = React.createClass({
       </li>
     });
   },
-  onChangexxx: function(event, topics) {
+  onChange: function(event, topics) {
     this.setState({topics: topics})
   }
 });
